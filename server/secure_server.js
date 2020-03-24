b58361ce2fad8c0e472f87b6a3174844c08ceaa6
sameersitre <sameersitre@icloud.com>
2020-03-24 21:34:11 +0530

    prod

--- server/secure_server.js
+++ server/secure_server.js
@@ -1,32 +1,48 @@
-/* var https = require('https');
 
+/* ************ */
+
+var fs = require('fs');
+
+var https = require("https");
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var cors = require('cors')
+
+//start mysql connection
+// var connection = mysql.createConnection({
+//     host: 'localhost', //mysql database host name
+//     user: 'root', //mysql database user name
+//     password: 'root', //mysql database password
+//     database: 'DynamicProto' //mysql database name
+// });
+
+// connection.connect(function (err) {
+//     if (err) throw err
+//     console.log('You are now connected with mysql database...')
+// })
+//end mysql connection
+
+//start body-parser configuration
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+//end body-parser configuration
+app.use(cors())
 var options = {
     key: fs.readFileSync('./certs/server-key.pem'),
     cert: fs.readFileSync('./certs/server-cert.pem'),
 };
-var server = https.createServer(options, app).listen(port, function () {
-    console.log("Express server listening on port " + port);
-}); */
-/* ************ */
-var fs = require('fs'),
-    http = require('http'),
-    https = require('https'),
-    express = require('express');
-
-var port = 8000;
- 
-const options = {
-    key: fs.readFileSync(__dirname + '/certs/server-key.pem', 'utf8'),
-   cert: fs.readFileSync(__dirname + '/certs/server-cert.pem', 'utf8')
- };
-var app = express();
-
- https.createServer(options, app).listen(port, function(){
-  console.log("Express server listening on port " + port);
+https.createServer(options, app).listen(8000, "127.0.0.1", function () {
+    
+    console.log("Express server listening on  http:// ",8000);
 });
 
+//rest api to get all customers
 app.get('/getDetails', function (req, res) {
-    res.writeHead(200);
-    console.log("object")
-    res.end("hello world\n");
+     res.end('JSON.stringify(results)');
+    
 });
